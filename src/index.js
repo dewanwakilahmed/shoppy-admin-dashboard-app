a19f9c89ce8c6df4319653565ea76c3f7afe11a0
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-06-06 21:00:29 +0600

    Added Syncfusion Key

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import App from "./App";
 
 import reportWebVitals from "./reportWebVitals";
 
+import { registerLicense } from "@syncfusion/ej2-base";
+
+registerLicense(process.env.REACT_APP_SyncFusionKey);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
