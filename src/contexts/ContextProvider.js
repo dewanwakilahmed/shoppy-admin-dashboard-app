744df20bd2ff589c89f9729e13868bcccc50890a
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-06-09 20:36:24 +0600

    Revised Context API

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -2,32 +2,55 @@ import React, { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
 
-const initialState = {
-  chat: false,
+const navBarItemsState = {
   cart: false,
-  userProfile: false,
+  chat: false,
   notification: false,
+  userProfile: false,
 };
 
 export const ContextProvider = ({ children }) => {
-  const [activeMenu, setActiveMenu] = useState(true);
-  const [isClicked, setIsClicked] = useState(initialState);
-  const [screenSize, setScreenSize] = useState(undefined);
+  const [currentScreenSize, setCurrentScreenSize] = useState(undefined);
+  const [toggleMenu, setToggleMenu] = useState(true);
+  const [toggleNavbarItem, setToggleNavbarItem] = useState(navBarItemsState);
+  const [toggleThemeSettings, setToggleThemeSettings] = useState(false);
+  const [currentThemeMode, setCurrentThemeMode] = useState("Dark");
+  const [currentThemeColor, setCurrentThemeColor] = useState("#03C9D7");
 
-  const handleClick = (clicked) => {
-    setIsClicked({ ...initialState, [clicked]: true });
+  const setThemeMode = (e) => {
+    let themeMode = e.target.value;
+    setCurrentThemeMode(themeMode);
+    localStorage.setItem("themeMode", themeMode);
+  };
+
+  const setThemeColor = (color) => {
+    let themeColor = color;
+    setCurrentThemeColor(themeColor);
+    localStorage.setItem("themeColor", themeColor);
+  };
+
+  const toggleOnClick = (clickedItem) => {
+    setToggleNavbarItem({ ...navBarItemsState, [clickedItem]: true });
   };
 
   return (
     <StateContext.Provider
       value={{
-        activeMenu,
-        setActiveMenu,
-        isClicked,
-        setIsClicked,
-        handleClick,
-        screenSize,
-        setScreenSize,
+        currentScreenSize,
+        setCurrentScreenSize,
+        toggleMenu,
+        setToggleMenu,
+        toggleNavbarItem,
+        setToggleNavbarItem,
+        toggleThemeSettings,
+        setToggleThemeSettings,
+        currentThemeMode,
+        setCurrentThemeMode,
+        currentThemeColor,
+        setCurrentThemeColor,
+        setThemeMode,
+        setThemeColor,
+        toggleOnClick,
       }}
     >
       {children}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// CSS
 import "./index.css";
 
+// MAIN APP COMPONENT
 import App from "./App";
 
 // Context API
@@ -10,8 +12,8 @@ import { ContextProvider } from "./contexts/ContextProvider";
 
 import reportWebVitals from "./reportWebVitals";
 
+// Syncfusion License Registration
 import { registerLicense } from "@syncfusion/ej2-base";
-
 registerLicense(process.env.REACT_APP_SyncFusionKey);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
