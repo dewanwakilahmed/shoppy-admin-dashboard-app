81ac6617aba7bb50a5f09f29f4c915c3b416581f
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-06-10 15:03:44 +0600

    Setup Browser Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { BrowserRouter } from "react-router-dom";
+
 // CSS
 import "./index.css";
 
@@ -20,7 +22,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ContextProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ContextProvider>
   </React.StrictMode>
 );
